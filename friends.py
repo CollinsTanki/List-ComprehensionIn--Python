ccb8f9dc06899ccfdfb8370903b51915ed25dcef
Collins Tanki <51945008+CollinsTanki@users.noreply.github.com>
2026-03-20 20:39:43 +0300

    A simple example using Object Dictionary

---
+++ friends.py
@@ -0,0 +1,23 @@
+friends = [
+  {
+    "name": "Rizwan",
+    "Registered": True,
+    "Phone": "0724458616"
+  },
+  {
+    "name": "Collins",
+    "Registered": False,
+    "Phone": "07579436377"
+  },
+  {
+    "name": "Rayyan",
+    "Registered": True,
+    "Phone": "0710566212"
+  },
+  {
+    "name": "Tanki",
+    "Registered": False,
+    "Phone": "0724458616"
+  },
+
+]
